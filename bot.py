522828999232600946a3ae8e52b7f07333d5788b
Brandon <xMercuryxD@gmail.com>
2020-01-02 00:14:09 -0500

    updated bot

--- bot.py
+++ bot.py
@@ -17,12 +17,21 @@ async def on_ready():
     print(f'Logged in to Discord as {client.user}')
 
 @client.event
-async def on_message(message):
+async def on_message(message:discord.Message):
     if message.author == client.user:
         return
     
     if message.content.startswith('!test'):
-        print(f'{message.author} has activated command')
+        print(f'{message.author} has activated test')
         await message.channel.send(f'ur a bitch {message.author.mention}')
+    
+    if message.content.startswith('!paramtest'):
+        print(f'{message.author} has activated paramtest')
+        params = message.content.split(' ')
+
+        if params[1] == 'baka':
+            await message.channel.send(f'ur a fookin baka {message.author.mention}')
+        elif params[1] == 'tits':
+            await message.channel.send(f'( . Y . ) {message.author.mention}')
 
 client.run(token)
